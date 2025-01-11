5d23cf8139397ac0bb9ca8127573e7f38d4de6c4
Emir Karabeg <emirkarabeg@berkeley.edu>
2025-01-11 13:06:47 -0800

    Style change on workflow block

--- app/w/components/blocks/components/workflow-block/workflow-block.tsx
+++ app/w/components/blocks/components/workflow-block/workflow-block.tsx
@@ -67,7 +67,7 @@ export function WorkflowBlock({
         <span className="font-medium text-md">{name}</span>
       </div>
 
-      <div className="px-4 pt-2 pb-4 space-y-4">
+      <div className="px-4 pt-3 pb-4 space-y-4">
         {subBlockRows.map((row, rowIndex) => (
           <div key={`row-${rowIndex}`} className="flex gap-4">
             {row.map((subBlock, blockIndex) => (
