145bd0cca66360842aa2f28567608f71b99b24e8
Waleed Latif <walif6@gmail.com>
2025-03-13 13:10:50 -0700

    feat(docs): added more docs, cleaned up

---
+++ docs/lib/utils.ts
@@ -0,0 +1,9 @@
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+/**
+ * Combines multiple class names into a single string, merging Tailwind classes properly
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
