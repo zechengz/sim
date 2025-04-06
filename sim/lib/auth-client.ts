2de6f45f9ade3d65b22918e64cda0ec5966a221b
Waleed Latif <walif6@gmail.com>
2025-04-06 01:07:52 -0700

    improvement: dynamically set better auth url based on env

--- sim/lib/auth-client.ts
+++ sim/lib/auth-client.ts
@@ -1,7 +1,25 @@
 import { emailOTPClient, genericOAuthClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 
+export function getBaseURL() {
+  let baseURL
+
+  if (process.env.VERCEL_ENV === 'preview') {
+    baseURL = `https://${process.env.VERCEL_URL}`
+  } else if (process.env.VERCEL_ENV === 'development') {
+    baseURL = `https://${process.env.VERCEL_URL}`
+  } else if (process.env.VERCEL_ENV === 'production') {
+    baseURL = process.env.BETTER_AUTH_URL
+  } else if (process.env.NODE_ENV === 'development') {
+    baseURL = process.env.BETTER_AUTH_URL
+  }
+
+  console.log('baseURL:', baseURL)
+  return baseURL
+}
+
 export const client = createAuthClient({
+  baseURL: getBaseURL(),
   plugins: [genericOAuthClient(), emailOTPClient()],
 })
 export const { useSession } = client
