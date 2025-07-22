919cf0b58e1c847fc33fe8c2bbbb2707e788b5e6
Waleed Latif <walif6@gmail.com>
2025-07-21 20:17:00 -0700

    improvement(docs): updated docs with new videos, new tools (#744)
    * updated docs with new videos, new tools
    
    * update typeform icon

--- apps/sim/components/icons.tsx
+++ apps/sim/components/icons.tsx
@@ -1853,15 +1853,22 @@ export function TypeformIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
-      width='24'
-      height='24'
-      viewBox='0 0 24 24'
-      fill='none'
+      version='1.1'
+      id='Layer_1'
       xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+      x='0px'
+      y='0px'
+      viewBox='0 0 122.3 80.3'
+      xmlSpace='preserve'
     >
-      <g transform='translate(1, 4)'>
-        <rect x='0' y='0' width='5' height='16' rx='2.5' fill='currentColor' />
-        <rect x='8' y='0' width='14' height='16' rx='4' fill='currentColor' />
+      <g>
+        <path
+          fill='currentColor'
+          d='M94.3,0H65.4c-26,0-28,11.2-28,26.2l0,27.9c0,15.6,2,26.2,28.1,26.2h28.8c26,0,28-11.2,28-26.1V26.2
+		C122.3,11.2,120.3,0,94.3,0z M0,20.1C0,6.9,5.2,0,14,0c8.8,0,14,6.9,14,20.1v40.1c0,13.2-5.2,20.1-14,20.1c-8.8,0-14-6.9-14-20.1
+		V20.1z'
+        />
       </g>
     </svg>
   )
